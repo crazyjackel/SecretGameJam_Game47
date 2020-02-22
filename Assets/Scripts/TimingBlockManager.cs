a70366c7c28e02ffd93f0c0edb0dc8d2e4e7581a
crazyjackel <jackel1020@gmail.com>
2020-02-22 13:39:08 -0500

    acwas

---
+++ Assets/Scripts/TimingBlockManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimingBlockManager : MonoBehaviour
+{
+    public static TimingBlockManager Instance;
+
+    private List<TimingDisableBlock> blocks = new List<TimingDisableBlock>();
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public static TimingBlockManager GetInstance()
+    {
+        return Instance;
+    }
+
+    public void AddTimingBlock(TimingDisableBlock block)
+    {
+        blocks.Add(block);
+    }
+
+    bool previous;
+    bool onOff = true;
+    void Update()
+    {
+        if (previous != GameManager.GetInstance().movement.IsGrounded && !GameManager.GetInstance().movement.IsGrounded)
+        {
+            onOff = !onOff;
+            foreach (TimingDisableBlock block in blocks)
+            {
+                block.gameObject.SetActive(onOff);
+            }
+        }
+
+        previous = GameManager.GetInstance().movement.IsGrounded;
+
+    }
+}
---
+++ Assets/Scripts/TimingDisableBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimingDisableBlock : MonoBehaviour
+{
+    public void Start()
+    {
+        TimingBlockManager.GetInstance().AddTimingBlock(this);
+    }
+    bool previous;
+    void Update()
+    {
+        if (previous != GameManager.GetInstance().movement.IsGrounded)
+        {
+            this.enabled = !this.enabled;
+        }
+                
+        previous = GameManager.GetInstance().movement.IsGrounded;
+
+    }
+}
