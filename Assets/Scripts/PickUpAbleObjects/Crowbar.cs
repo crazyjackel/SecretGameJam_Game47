c391bc191e3b0cefbc33005e868d1068d1d31d44
crazyjackel <jackel1020@gmail.com>
2020-02-21 20:12:26 -0500

    Item Pickup part 1

---
+++ Assets/Scripts/PickUpAbleObjects/Crowbar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowbar : PickupAbleItem
+{
+    public override void OnPickUp()
+    {
+        this.transform.parent = PickUpManager.GetInstance().guiCamera.transform;
+        this.transform.position = PickUpManager.GetInstance().EmptyPosition.transform.position;
+        this.transform.position += this.AdjustmentWhenHeld.position;
+        this.transform.rotation = this.AdjustmentWhenHeld.rotation;
+        this.transform.localScale = this.AdjustmentWhenHeld.localScale;
+    }
+
+}
+
--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -10,6 +10,9 @@ public class PlayerCamera : MonoBehaviour
     public float smoothing = 2.0f;
 
     public GameObject player;
+    public LayerMask layer;
+
+    public PickupAbleItem heldItem = null;
 
     // Use this for initialization
     void Start()
@@ -31,5 +34,29 @@ public class PlayerCamera : MonoBehaviour
 
         transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
         player.transform.rotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+
+        //Does the Pickup and Drop System
+        if (Input.GetKeyDown(KeyCode.E) && heldItem == null)
+        {
+            Physics.Raycast(this.transform.position, this.transform.forward, out RaycastHit hit, 10.0f, layer);
+            if (hit.collider != null)
+            {
+                PickupAbleItem item = hit.collider.gameObject.GetComponent<PickupAbleItem>();
+                if (item != null)
+                {
+                    heldItem = item;
+                    heldItem.OnPickUp();
+                }
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            heldItem.OnDrop();
+        }
+
+        if (Input.GetMouseButtonDown(0) && heldItem != null)
+        {
+            heldItem.OnClick();
+        }
     }
 }
