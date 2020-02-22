38516b64301e1ec84b336b3e26b4813d1af93c9a
crazyjackel <jackel1020@gmail.com>
2020-02-21 20:12:35 -0500

    part 2

---
+++ Assets/Scripts/PickupAbleItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PickupAbleItem : MonoBehaviour
+{
+    public Transform AdjustmentWhenHeld;
+    public virtual void OnPickUp()
+    {
+
+    }
+
+    public virtual void OnClick()
+    {
+
+    }
+
+    public virtual void OnDrop()
+    {
+
+    }
+}
